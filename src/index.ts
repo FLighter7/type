d61a4c1c746aa07aef31dc606d359775e38af239
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-03-29 01:45:26 +0300

    feat: add isStringifiedUndefined

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './getType.js';
 
 // Undefined
 export * from './isUndefined.js';
+export * from './isStringifiedUndefined.js';
---
+++ src/isStringifiedUndefined.ts
@@ -0,0 +1,7 @@
+/**
+ * Checks that a value is `'undefined'` (as string)
+ *
+ * @param {unknown} val
+ * @returns {boolean}
+ */
+export const isStringifiedUndefined = (val: unknown): boolean => val === 'undefined';
