9351da884adfae8b6d8b806f16049a1da72649b6
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-07-08 00:04:00 +0300

    feat: change isSymbol

--- src/isSymbol.ts
+++ src/isSymbol.ts
@@ -1,9 +1,7 @@
-import {getType} from './getType.js';
-
 /**
  * Checks that a value is `Symbol`
  *
  * @param {unknown} val any value
  * @returns {boolean}
  */
-export const isSymbol = (val: unknown): boolean => getType(val) === 'symbol';
+export const isSymbol = (val: unknown): boolean => typeof val === 'symbol';
