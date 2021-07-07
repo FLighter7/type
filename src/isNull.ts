3dcc03338eb003c3310e1974df00b66cf500835c
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-07-08 00:00:44 +0300

    feat: change isNull

--- src/isNull.ts
+++ src/isNull.ts
@@ -1,12 +1,26 @@
-import {isStringifiedNull} from './isStringifiedNull.js';
+import {_isSuitable, FlagsType} from './_isSuitable.js';
+
+const n = 'null';
 
 /**
- * Checks that a value is `null`
+ * Checks that a value is `null` or `'null'` (as string)
  *
  * @param {unknown} val any value
- * @param {boolean} [checkString = false] also check if a value is `'null'` (as string)
+ * @param {FlagsType} [flags] CHECK_STRING or CHECK_STRING_CASE_INSENSITIVE
  * @returns {boolean}
  */
-export const isNull = (val: unknown, checkString = false): boolean => {
-  return val === null || (checkString && isStringifiedNull(val));
+export const isNull = (val: unknown, flags?: FlagsType): boolean => {
+  const preliminaryResult = val === null;
+
+  // This condition is faster than in _isSuitable
+  if (!flags) {
+    return preliminaryResult;
+  }
+
+  return _isSuitable({
+    preliminaryResult,
+    val,
+    flags,
+    rightCases: n,
+  });
 };
