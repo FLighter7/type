ce93b318eafc6765b7fa8825948888a17be03514
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-07-09 23:15:08 +0300

    feat: change isString

--- src/isString.ts
+++ src/isString.ts
@@ -1,4 +1,4 @@
-import {getType} from './getType.js';
+import {_isSuitable, FlagsType} from './_isSuitable.js';
 
 /**
  * Checks that a value is string
@@ -6,6 +6,20 @@ import {getType} from './getType.js';
  * Note: `new String` **is not** a string, it's an object
  *
  * @param {unknown} val any value
+ * @param {FlagsType} [flags] STRING_*
  * @returns {boolean}
  */
-export const isString = (val: unknown): boolean => getType(val) === 'string';
+export const isString = (val: unknown, flags?: FlagsType): boolean => {
+  const preliminaryResult = typeof val === 'string';
+
+  // This condition is faster than in _isSuitable
+  if (!flags) {
+    return preliminaryResult;
+  }
+
+  return _isSuitable({
+    preliminaryResult,
+    val,
+    flags,
+  });
+};
