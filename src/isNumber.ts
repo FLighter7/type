2b7d9ea92d86e5166bee661c3ea3bd7368bf72f3
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-07-21 19:54:57 +0300

    feat: change isNumber

---
+++ src/isNumber.ts
@@ -0,0 +1,79 @@
+/**
+ * +value === Number(value)
+ * @see https://stackoverflow.com/a/17106702/8165331
+ */
+
+import {_isSuitable, FlagsType} from './_isSuitable.js';
+import {FunctionsKeys} from './flags.js';
+
+/**
+ * Inner function: checks that a value is number and not `NaN`
+ *
+ * @param {unknown} val
+ * @returns {boolean}
+ */
+const _isNumber = (val: unknown): boolean => typeof val === 'number' && val === val;
+
+/**
+ * Checks that a value is number and not `NaN`
+ *
+ * Note: `new Number` **is not** a number, it's an object
+ *
+ * @param {unknown} val any value
+ * @param {FlagsType} [flags] CHECK_STRING, CHECK_STRING_CASE_INSENSITIVE, NUMBER_*
+ * @param {boolean} [emptyStringIsNumber = false] if `true` then an empty string or string with spaces only
+ * are equal to number and the result for them is `true`
+ * @returns {boolean}
+ */
+export const isNumber = (val: unknown, flags?: FlagsType, emptyStringIsNumber = false): boolean => {
+  let preliminaryResult = _isNumber(val);
+
+  // This condition is faster than in _isSuitable
+  if (!flags) {
+    return preliminaryResult;
+  }
+
+  // Flags to array
+  const flagsArray = Array.isArray(flags) ? flags : [flags];
+
+  // Check as string
+  const indexString = flagsArray.indexOf(FunctionsKeys.CHECK_STRING);
+  const indexStringInsensitive = flagsArray.indexOf(FunctionsKeys.CHECK_STRING_CASE_INSENSITIVE);
+  if (indexString >= 0 || indexStringInsensitive >= 0) {
+    if (typeof val !== 'string') {
+      return false;
+    }
+
+    // Trim spaces
+    if (!emptyStringIsNumber) {
+      val = val.trim();
+      if ((val as string).length === 0) {
+        return false;
+      }
+    }
+
+    // Special case for case insensitive "infinity"
+    const indexInfinity = flagsArray.indexOf(FunctionsKeys.NUMBER_INFINITE);
+    if (indexStringInsensitive >= 0 && indexInfinity >= 0) {
+      if (/^[+-]?infinity$/i.test(val as string)) {
+        val = (val as string).toLowerCase().replace(/i/, 'I');
+        delete flagsArray[indexInfinity];
+      } else {
+        return false;
+      }
+    }
+
+    val = +val;
+    preliminaryResult = _isNumber(val);
+
+    // Do not change the length of flags array
+    (indexString >= 0) && (delete flagsArray[indexString]);
+    (indexStringInsensitive >= 0) && (delete flagsArray[indexStringInsensitive]);
+  }
+
+  return _isSuitable({
+    preliminaryResult,
+    val,
+    flags: flagsArray,
+  });
+};
--- src/isStringifiedInfinity.ts
+++
@@ -1,9 +0,0 @@
-const inf = 'Infinity';
-
-/**
- * Checks that a value is `'Infinity'` or `'+Infinity'` or `'-Infinity'` (as string)
- *
- * @param {unknown} val any value
- * @returns {boolean}
- */
-export const isStringifiedInfinity = (val: unknown): boolean => val === inf || val === '+'+inf || val === '-'+inf;
