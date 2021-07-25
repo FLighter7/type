0ec7d151543b3475f31c9205e518ed00dccee95c
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-07-26 01:14:54 +0300

    test: change function

--- tests/test-values/function.ts
+++ tests/test-values/function.ts
@@ -4,25 +4,21 @@ export const FunctionValues: Value[] = [
   {
     value: () => {},
     type: 'function',
-    strictType: 'function',
     passableFunctions: ['isFunction'],
   },
   {
     value: function(){},
     type: 'function',
-    strictType: 'function',
     passableFunctions: ['isFunction'],
   },
   {
     value: new Function(),
     type: 'function',
-    strictType: 'function',
     passableFunctions: ['isFunction'],
   },
   {
     value: class{},
     type: 'function',
-    strictType: 'function',
     passableFunctions: ['isFunction'],
   },
 ]
