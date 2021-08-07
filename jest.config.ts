de871600a73b050a359755b678dd466148543b02
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-08-08 00:25:13 +0300

    build: fix umd initialization

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
+import {defaults} from 'jest-config';
 import type {Config} from '@jest/types';
 
 export default <Config.InitialOptions>{
   preset: 'ts-jest',
   testRegex: 'tests/.+\\.test\\.ts$',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'cjs', 'mjs'],
 }
