344c0509f4d11c5ed2708acc33a231fc657951d5
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-09-04 21:13:58 +0300

    feat: add a new flag OR
    - add realization
    - except numbers and bigint

--- src/_isSuitable.ts
+++ src/_isSuitable.ts
@@ -4,6 +4,9 @@
 
 import {Functions, TypeFlags, FunctionsKey} from './flags.js';
 
+const checkString = TypeFlags.CHECK_STRING;
+const checkStringInsensitive = TypeFlags.CHECK_STRING_CASE_INSENSITIVE;
+
 // Export this type here to avoid two imports in other files
 export type FlagsType = FunctionsKey | FunctionsKey[];
 
@@ -21,26 +24,72 @@ export const _isSuitable = (
   // Flags to array
   flags = Array.isArray(flags) ? flags : [flags];
 
-  // If the result is false and we don't have flags for checking strings
-  if (!preliminaryResult
-    && !flags.includes(TypeFlags.CHECK_STRING)
-    && !flags.includes(TypeFlags.CHECK_STRING_CASE_INSENSITIVE)
+  // Has CHECK_STRING, CHECK_STRING_CASE_INSENSITIVE, OR
+  let hasString = false;
+  let hasStringInsensitive = false;
+  let hasCheckString = false;
+  let hasOr = false;
+
+  for (let i = 0; i < flags.length; i++) {
+    const flag = flags[i];
+
+    if (!hasString && flag === checkString) {
+      hasString = true;
+      delete flags[i];
+    } else if (!hasStringInsensitive && flag === checkStringInsensitive) {
+      hasStringInsensitive = true;
+      delete flags[i];
+    } else if (!hasOr && flag === TypeFlags.OR) {
+      hasOr = true;
+      delete flags[i];
+    }
+
+    hasCheckString = hasString || hasStringInsensitive;
+    if (hasCheckString && hasOr) {
+      break;
+    }
+  }
+
+  /**
+   * 4 cases:
+   * 1. has check string, has or
+   * 2. has check string, has not or
+   * 3. has not check string, has or
+   * 4. has not check string, has not or
+   *
+   * *If it has not check string then "OR" doesn't matter
+   */
+  // Right cases to array and check as string
+  let isNeededString = false;
+  if (hasCheckString) {
+    rightCases = Array.isArray(rightCases) ? rightCases : [rightCases];
+    isNeededString = hasString
+      ? Functions[checkString](val, rightCases)
+      : Functions[checkStringInsensitive](val, rightCases);
+  }
+
+  if (
+    // 1
+    (hasCheckString && hasOr && !preliminaryResult && !isNeededString)
+    // 2 - preliminary result doesn't matter, check string only
+    || (hasCheckString && !hasOr && !isNeededString)
+    // 3, 4 - if the result is false then return it (do not check any other flags)
+    || (!hasCheckString && !preliminaryResult)
   ) {
     return false;
   }
 
-  // The result is true and we need to check flags
-  // Or
-  // The result is false, but we have to check string cases
-  rightCases = Array.isArray(rightCases) ? rightCases : [rightCases];
+  // 1 - the result or string result is true then check other flags
+  // 2 - the string result is true then check other flags
+  // 3, 4 - the result is true then check other flags
   for (const flag of flags) {
-    const f = Functions[flag] as (val: unknown, rightCases: string[]) => boolean;
+    const f = Functions[flag] as (val: unknown) => boolean;
     // Invalid flag
     if (!f) {
       continue;
     }
 
-    const valid = f(val, rightCases);
+    const valid = f(val);
     if (!valid) {
       return false;
     }
