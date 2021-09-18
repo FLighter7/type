5d1167c2fca167fa3f96eb28d4926088e6c0d3ec
FLighter7 <21360067+FLighter7@users.noreply.github.com>
2021-09-19 00:25:07 +0300

    feat: add a new flag OR
    - add realization for bigint

--- src/isBigInt.ts
+++ src/isBigInt.ts
@@ -1,11 +1,11 @@
-import {FlagsType} from './_isSuitable.js';
-import {TypeFlags} from './flags.js';
+import {FlagsType, _isSuitable} from './_isSuitable.js';
+import {_getCommonFlags} from './_getCommonFlags.js';
 
 /**
  * Checks that a value is a big integer
  *
  * @param {unknown} val any value
- * @param {FlagsType} [flags] CHECK_STRING or CHECK_STRING_CASE_INSENSITIVE
+ * @param {FlagsType} [flags] CHECK_STRING, CHECK_STRING_CASE_INSENSITIVE, OR, NUMBER_*
  * @returns {boolean}
  */
 export const isBigInt = (val: unknown, flags?: FlagsType): boolean => {
@@ -17,24 +17,41 @@ export const isBigInt = (val: unknown, flags?: FlagsType): boolean => {
   }
 
   // Flags to array
-  const flagsArray = Array.isArray(flags) ? flags : [flags];
+  flags = Array.isArray(flags) ? flags : [flags];
 
-  // As string
-  if (
-    flagsArray.includes(TypeFlags.CHECK_STRING)
-    || flagsArray.includes(TypeFlags.CHECK_STRING_CASE_INSENSITIVE)
-  ) {
-    if (typeof val !== 'string') {
-      return false;
-    }
+  // Has CHECK_STRING, CHECK_STRING_CASE_INSENSITIVE, OR
+  const [, , hasCheckString, hasOr] = _getCommonFlags(flags);
 
+  /**
+   * 4 cases:
+   * 1. has check string, has or
+   * 2. has check string, has not or
+   * 3. has not check string, has or
+   * 4. has not check string, has not or
+   *
+   * *If it has not check string then "OR" doesn't matter
+   */
+  // Check as string
+  let isNeededString = false;
+  if (hasCheckString && typeof val === 'string') {
     try {
-      const n = BigInt(val);
-      return n > Number.MAX_SAFE_INTEGER || n < Number.MIN_SAFE_INTEGER;
+      val = BigInt(val);
+      isNeededString = val > Number.MAX_SAFE_INTEGER || val < Number.MIN_SAFE_INTEGER;
     } catch (e) {
-      return false;
+      // Useless comment for eslint
     }
   }
 
-  return preliminaryResult;
+  if (
+    // 1
+    (hasCheckString && hasOr && !preliminaryResult && !isNeededString)
+    // 2 - preliminary result doesn't matter, check string only
+    || (hasCheckString && !hasOr && !isNeededString)
+    // 3, 4 - if the result is false then return it (do not check any other flags)
+    || (!hasCheckString && !preliminaryResult)
+  ) {
+    return false;
+  }
+
+  return _isSuitable(true, val, flags);
 };
